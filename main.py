be005a78b4db61555e3729d5ac63961b4b82cc94
Gleb Tcivie <gleb@tcivie.com>
2022-06-13 12:51:16 +0300

    Added basic bot functionality

--- main.py
+++ main.py
@@ -3,18 +3,22 @@
 An application that checks from the list of the products if some price or aviability has changed and notifies via Telegram bot
 """
 
-import asyncio
-import telegram
+import telebot
 import json
 
-
-async def main():
-    with open('Credentials', 'r') as file:
-        credentials = json.load(file)
-        bot = telegram.Bot(credentials['TOKEN'])
-        async with bot:
-            print((await bot.get_updates())[0])
+with open('Credentials') as file:
+    credentials = json.load(file)
+bot = telebot.TeleBot(credentials['TOKEN'], parse_mode=None)  # You can set parse_mode by default. HTML or MARKDOWN
 
 
-if __name__ == '__main__':
-    asyncio.run(main())
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
